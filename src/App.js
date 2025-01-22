00f90c6801ae7b525092e63b294a7d3dca51429a
recursiveElk <finneom@gmail.com>
2025-01-23 11:35:11 +1300

    latest

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
 import './App.css';
 import ImportokWizard from '@importok/react';
-import React from 'react';
+import React, {useState} from 'react';
+import {Dropzone} from './Dropzone'
 
-const uploadedFile = new File(
-  [
-    'id,first_name,last_name,email,phone,address,country' + "\n",
-    '1,Jarred,Barton,Ricky.Stroman@gmail.com,605-747-6709,46709 Travon Gateway,EG' + "\n",
-    '2,Ivory,Yundt,Desiree2@gmail.com,459-727-9328 x788,1032 Haley Point,TF' + "\n",
-    '3,Randy,Bruen,Isobel.Wisoky@hotmail.com,364-899-5352,972 Fadel Shores,AI' + "\n",
-  ],
-  'contacts.csv',
-  { type: 'text/csv' }
-);
+// const demoUploadedFile = new File(
+//   [
+//     'id,first_name,last_name,email,phone,address,country' + "\n",
+//     '1,Jarred,Barton,Ricky.Stroman@gmail.com,605-747-6709,46709 Travon Gateway,EG' + "\n",
+//     '2,Ivory,Yundt,Desiree2@gmail.com,459-727-9328 x788,1032 Haley Point,TF' + "\n",
+//     '3,Randy,Bruen,Isobel.Wisoky@hotmail.com,364-899-5352,972 Fadel Shores,AI' + "\n",
+//   ],
+//   'contacts.csv',
+//   { type: 'text/csv' }
+// );
+
+const demoFields = {
+  first_name: {
+    label: 'First Name',
+    transformers: 'capitalize|trim'
+  },
+  last_name: {
+    label: 'Last Name',
+    validators: 'required|length:5',
+    transformers: 'capitalize|trim'
+  },
+  company_name: {
+    label: 'Company Name',
+    transformers: 'trim'
+  },
+  email: {
+    label: 'Email',
+    validators: 'email|required',
+    transformers: 'lowercase|trim'
+  },
+  phone: {
+    label: 'Phone',
+    validators: 'phone',
+    transformers: 'replace:-|trim'
+  },
+  country: {
+    label: 'Country',
+    validators: 'in:CY,GR,UK',
+    transformers: 'uppercase|trim',
+  },
+};
 
 function App() {
-  const fields = {
-    first_name: {
-      label: 'First Name',
-      transformers: 'capitalize|trim'
-    },
-    last_name: {
-      label: 'Last Name',
-      validators: 'required|length:5',
-      transformers: 'capitalize|trim'
-    },
-    company_name: {
-      label: 'Company Name',
-      transformers: 'trim'
-    },
-    email: {
-      label: 'Email',
-      validators: 'email|required',
-      transformers: 'lowercase|trim'
-    },
-    phone: {
-      label: 'Phone',
-      validators: 'phone',
-      transformers: 'replace:-|trim'
-    },
-    country: {
-      label: 'Country',
-      validators: 'in:CY,GR,UK',
-      transformers: 'uppercase|trim',
-    },
-  };
+
+  const [uploadedFile, setUploadedFile] = useState(undefined);
 
   const saveRecord = async function (record, meta) {
     // Fake a network request and throw an error randomly
@@ -54,12 +58,17 @@ function App() {
       throw new Error(`Response status ${response.status}`);
     }
   };
+  console.log('uploadedFile', uploadedFile)
 
   return (
     <div className="App">
+      <Dropzone onFileUploaded={(acceptedFiles) => {
+        console.log('acceptedFiles', acceptedFiles)
+        setUploadedFile(acceptedFiles[0])
+      }}/>
       <ImportokWizard
         title="ImportOK Example for React"
-        fields={fields}
+        fields={demoFields}
         sampleFile="/sample.csv"
         onRecordReady={saveRecord}
         uploadedFile={uploadedFile}
---
+++ src/Dropzone.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import {useDropzone} from 'react-dropzone';
+import './Dropzone.css'
+
+export function Dropzone(props) {
+  const {onFileUploaded} = props;
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+    accept: {
+        'text/csv': ['.csv']
+    },
+    maxFiles:2
+  });
+  
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  useEffect(() => {
+    onFileUploaded?.(acceptedFiles)
+  }, [acceptedFiles, onFileAccept])
+
+  return (
+    <section className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+}
+
